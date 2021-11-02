15848b2efd755ca165551ed5ac42ee0febcfab07
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2021-11-02 12:26:47 +0530

    notes app v1

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Cloning and starting application
+1. - git clone https://github.com/iamVaibhav100/react-notes-app
+1. - cd react-notes-app
+2. - npm install
+3. - npm run server //JSON SERVER(if you have'nt installed json server just run npm install -g json-server)
+4. - npm start  //REACT SERVER
+
+
+# Notes List
+<img src="./Notes.PNG">  
+
---
+++ src/assets/data.js
@@ -0,0 +1,17 @@
+let notes = [
+    {
+        "id": 1,
+        "body": "I-do\n\n- React app made with ❤️ by Vaibhav.",
+        "updated": "2021-11-14T13:49:02.078653Z",
+        "created": "2021-11-13T21:54:16.235392Z"
+    },
+    {
+        "id": 2,
+        "body": "Click on ➕.",
+        "updated": "2021-07-13T20:43:18.550058Z",
+        "created": "2021-07-13T00:17:13.289897Z"
+    },
+]
+
+
+export default notes;
